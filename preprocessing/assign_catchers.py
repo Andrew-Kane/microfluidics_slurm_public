6b61780ad3f9f7b0975ab30af7783b1965aa20e9
Andrew-Kane <andrew.kane.25@gmail.com>
2018-03-28 20:49:16 -0400

    Added definitions of functions Oddyssey won't import.

--- preprocessing/assign_catchers.py
+++ preprocessing/assign_catchers.py
@@ -7,9 +7,8 @@ import matplotlib.colors as colors
 from skimage.external.tifffile import tifffile ## just latest version of tifffile
 from skimage import filters
 import argparse
-from microfluidics import catcher_finder
-from catcher_finder import match_template_to_image
-from catcher_finder import find_catchers
+from skimage.feature import peak_local_max
+from scipy.signal import fftconvolve
 
 
 parser = argparse.ArgumentParser(description = 'Identify catchers and write to \
@@ -36,6 +35,56 @@ bottom = args.bottom
 left = args.left
 right = args.right
 position = args.position
+
+def match_template_to_image(image, template, min_distance=25, threshold_rel=0.35):
+    """ Uses fourier transforms to find peaks in image and locate catchers
+    
+        image -- 2d np array in which to look for catchers
+        template -- 2d array with example of catcher -- cropped tight around catcher
+        returns a list of array of coordinates where template occured in the image"""
+    return peak_local_max(fftconvolve(image - np.mean(image),
+                                      template[::-1, ::-1] -
+                                      np.mean(template), 'same'),
+                          min_distance=min_distance, 
+                          threshold_rel=threshold_rel)
+    
+def find_catchers(timepoint,
+                  pos,
+                  name_format, # (pos, channel, time, filetype)
+                  dest_parent_dir,
+                  experiment_directory,
+                  channel = "BF",
+                  channel_number = 1):
+    '''Finds catchers in image and writes to new file
+    
+        timepoint -- the timepoint to find catchers in
+        pos -- the position to find catchers in
+        name_format -- the name format to search for files
+        dest_parent_dir -- directory to write out catcher locations
+        experimental directory -- directory from which to pull data
+        channel -- channel to search for catchers, default is BF
+        channel_number -- channel number to search for catchers, default is 1'''
+    tif_path = os.path.join(experiment_directory,
+                            'Pos%d' % pos,
+                             channel,
+                             name_format % (pos, "c%d%s" % (channel_number, channel), timepoint))
+    tif = tifffile.imread([tif_path])
+    if len(tif.shape) > 2:
+        tif = np.max(tif, axis=0)
+    blobs = match_template_to_image(image=tif[0:512, 0:512],
+                                    template=cropped_reference_catcher,
+                                    min_distance=100,
+                                    threshold_rel=0.20)
+    catcher_loc_path = os.path.join(
+        dest_parent_dir,
+        (name_format.split(".")[0]+".catcher_loc") %(pos, 
+                                    "c%d%s" % (channel_number, channel), 
+                                    timepoint))
+    with open(catcher_loc_path, "w") as catcher_locs_fout:
+        catcher_locs_fout.write('%s\t%s\n' % ('catcher_x_coord', 'catcher_y_coord'))
+        for blob in blobs:
+            catcher_locs_fout.write('%s\t%s\n' % (blob[1], blob[0]))
+    return (tif, blobs)
 
 def main():
     channel = 'BF'
